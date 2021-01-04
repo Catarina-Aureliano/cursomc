66187b5cfe7562824acc5690ace986b1eba0440b
Catarina Aureliano <catarina.aureliano@gmail.com>
2021-01-03 21:21:26 -0300

    Projeto criado

---
+++ src/main/java/com/catarinaaureliano/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.catarinaaureliano.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
