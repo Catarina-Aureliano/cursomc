7614a6c864ed9d870be342953cd31a38dc285cdb
Catarina Aureliano <catarina.aureliano@gmail.com>
2021-03-02 17:30:02 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/catarinaaureliano/cursomc/domain/Endereco.java
+++ src/main/java/com/catarinaaureliano/cursomc/domain/Endereco.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Endereco implements Serializable {
@@ -25,7 +25,7 @@ public class Endereco implements Serializable {
 	private String cep;
 	
 	//aqui vai as associações e cliente_id é a chave estrangeira
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
